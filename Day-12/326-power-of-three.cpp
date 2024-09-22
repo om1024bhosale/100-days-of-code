e7fd21a6e507b4b7bb5323ab1d0c961d87de7470
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-09-22 19:03:21 +0530

    Update 326-power-of-three.cpp

--- Day-12/326-power-of-three.cpp
+++ Day-12/326-power-of-three.cpp
@@ -17,3 +17,14 @@ public:
         return true;
     }
 };
+
+// Method 2:
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1) return true;
+        if(n<=0 || n%3) return false;
+        return isPowerOfThree(n/3);
+    }
+};
