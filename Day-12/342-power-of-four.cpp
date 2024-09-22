546e0eda5db236f614e970809bf52663a5c65249
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-09-22 19:09:28 +0530

    Create 342-power-of-four.cpp

---
+++ Day-12/342-power-of-four.cpp
@@ -0,0 +1,13 @@
+/*
+link -> https://leetcode.com/problems/power-of-four/description/
+*/
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1) return true;
+        if(n<=0 || n%4) return false;
+
+        return isPowerOfFour(n/4);
+    }
+};
